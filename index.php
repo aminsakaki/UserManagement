4f4bc205c102939dedd91d18ba7ec65e93f7f729
aminsakaki <aminsakaki63@gmail.com>
2024-05-31 15:51:45 +0330

    set autoload

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php 
 
+spl_autoload_register(function ($class) {
+    require __DIR__ . "/App/$class.php";
+});
+
 $parts = explode("/", $_SERVER["REQUEST_URI"]);
 if ($parts[2] != "users") {
     http_response_code(404);
@@ -7,3 +11,6 @@ if ($parts[2] != "users") {
 }
 
 $id = $parts[3] ?? null;
+
+$model=new UserGateway();
+$controller=new UserController($model);
